720d373a212793ccf19c08f36ab853df83a98fba
Yuwenbiao <ywb1996@189.cn>
2019-02-27 18:21:52 +0800

    采用顺序查询所有商店的方式实现的findPrices方法

--- src/com/company/CompletableFuture组合式异步编程/Future接口/使用CompletableFuture构建异步应用/Shop.java
+++ src/com/company/CompletableFuture组合式异步编程/Future接口/使用CompletableFuture构建异步应用/Shop.java
@@ -1,9 +1,12 @@
 package com.company.CompletableFuture组合式异步编程.Future接口.使用CompletableFuture构建异步应用;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * 商店
@@ -12,17 +15,39 @@ import java.util.concurrent.TimeUnit;
  * @date 19-2-26 下午12:05
  */
 public class Shop {
+    private String shopName;
+
+    public Shop(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
     public double getPrice(String product) {
         return calculatePrice(product);
     }
 
     public Future<Double> getPriceAsync(String product) {
-        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
-        new Thread(() -> {
-            double price = calculatePrice(product);
-            futurePrice.complete(price);
-        }).start();
-        return futurePrice;
+//        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+//        new Thread(() -> {
+//            try {
+//                double price = calculatePrice(product);
+//                futurePrice.complete(price);
+//            } catch (Exception ex) {
+//                //抛出CompletableFuture内的异常
+//                futurePrice.completeExceptionally(ex);
+//            }
+//        }).start();
+//        return futurePrice;
+
+        //使用工厂方法supplyAsync创建CompletableFuture对象
+        return CompletableFuture.supplyAsync(() -> calculatePrice(product));
     }
 
     private double calculatePrice(String product) {
@@ -42,7 +67,37 @@ public class Shop {
         }
     }
 
+    /**
+     * 使用异步API
+     */
     public static void main(String[] args) {
-        Shop shop = new Shop();
+        Shop shop = new Shop("test");
+        long start = System.nanoTime();
+        Future<Double> futurePrice = shop.getPriceAsync("my favorite product");
+        long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("Invocation returned after " + invocationTime + " msecs");
+
+        doSomethingElse();
+
+        try {
+            double price = futurePrice.get();
+            System.out.printf("Price is %.2f%n", price);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        long retrievaTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("Price returned after " + retrievaTime + " msecs");
+    }
+
+    private static void doSomethingElse() {
+        System.out.println("doSomethingElse");
+    }
+
+    /**
+     * 采用顺序查询所有商店的方式实现的findPrices方法
+     */
+    private List<String> findPrices(String product) {
+        List<Shop> shops = Arrays.asList(new Shop("BestPrice"), new Shop("LetsSaveBig"), new Shop("MyFavoriteShop"), new Shop("BuyItASll"));
+        return shops.stream().map(shop -> String.format("%s price is %.2f", shop.getShopName(), shop.getPrice(product))).collect(Collectors.toList());
     }
 }
